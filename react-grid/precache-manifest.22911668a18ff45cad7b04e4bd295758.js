ddabe64afd921e6b5db0b7660d45401f284b4931
Danil-Klimov <doink3690@gmail.com>
2019-09-23 17:34:40 +0300

    add react grid

---
+++ react-grid/precache-manifest.22911668a18ff45cad7b04e4bd295758.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fccf33bb261886e0b1cf",
+    "url": "/static/js/main.119058ea.chunk.js"
+  },
+  {
+    "revision": "10c95e8605f18490ee42",
+    "url": "/static/js/2.4a914921.chunk.js"
+  },
+  {
+    "revision": "fccf33bb261886e0b1cf",
+    "url": "/static/css/main.21d6fb87.chunk.css"
+  },
+  {
+    "revision": "d670a9059e14d926ec213c3e345bf7a3",
+    "url": "/index.html"
+  }
+];
